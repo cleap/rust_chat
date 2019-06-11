2f13091d3a9f3656f5ba735957b15f9f9136e413
cleap <cleapdev@gmail.com>
2019-06-11 13:33:48 -0600

    Changed to different tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,2 @@
-extern crate mio;
-use mio::*;
-
-// Implements WebSocketServer as an implementation of the Handler interface
-struct WebSocketServer;
-impl Handler for WebSocketServer {
-    type Timeout = usize;
-    type Message = ();
-}
-
 fn main() {
-    let mut event_loop = EventLoop::new().unwrap();
-    let mut handler = WebSocketServer;
-    event_loop.run(&mut handler).unwrap();
 }
